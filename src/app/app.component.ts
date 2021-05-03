fb2c2c83b1cc405532553dd22de0f0b3628cc98c
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-05-03 11:06:10 -0400

    adds an all movies button and skeloton for welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -10,6 +11,12 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
 
   constructor(public dialog: MatDialog) { }
 // this is the function that will open the dialog when the login button is clicked
